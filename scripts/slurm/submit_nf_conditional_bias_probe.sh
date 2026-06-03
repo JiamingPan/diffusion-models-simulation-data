bb1e3ef882d27f1fc34e2945c77f0361d7627041
Jiaming Pan <jiamingp@umich.edu>
2026-06-02 23:41:56 -0400

    Add prepare dependency for bias probe pipeline

--- scripts/slurm/submit_nf_conditional_bias_probe.sh
+++ scripts/slurm/submit_nf_conditional_bias_probe.sh
@@ -8,12 +8,14 @@ ACCOUNT=${ACCOUNT:-huterer2}
 
 cd "${PROJECT_DIR}"
 
-train128=$(sbatch -A "${ACCOUNT}" --parsable scripts/slurm/train_nf_conditional_bias_probe_n128.sbatch)
-train16k=$(sbatch -A "${ACCOUNT}" --parsable scripts/slurm/train_nf_conditional_bias_probe_n16384.sbatch)
-encoder=$(sbatch -A "${ACCOUNT}" --parsable scripts/slurm/train_nf_conditional_bias_encoder.sbatch)
+prepare=$(sbatch -A "${ACCOUNT}" --parsable scripts/slurm/prepare_nf_conditional_bias_probe.sbatch)
+train128=$(sbatch -A "${ACCOUNT}" --dependency=afterok:${prepare} --export=ALL,BIAS_PREPARE_IN_TRAIN=0 --parsable scripts/slurm/train_nf_conditional_bias_probe_n128.sbatch)
+train16k=$(sbatch -A "${ACCOUNT}" --dependency=afterok:${prepare} --export=ALL,BIAS_PREPARE_IN_TRAIN=0 --parsable scripts/slurm/train_nf_conditional_bias_probe_n16384.sbatch)
+encoder=$(sbatch -A "${ACCOUNT}" --dependency=afterok:${prepare} --parsable scripts/slurm/train_nf_conditional_bias_encoder.sbatch)
 sample=$(sbatch -A "${ACCOUNT}" --dependency=afterok:${train128}:${train16k} --parsable scripts/slurm/sample_nf_conditional_bias_probe.sbatch)
 eval_job=$(sbatch -A "${ACCOUNT}" --dependency=afterok:${sample}:${encoder} --parsable scripts/slurm/evaluate_nf_conditional_bias_probe.sbatch)
 
+echo "prepare:         ${prepare}"
 echo "train N=128:      ${train128}"
 echo "train N=16384:    ${train16k}"
 echo "encoder:          ${encoder}"
